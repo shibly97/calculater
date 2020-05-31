4bb287638c448d7367c30432a8d23f5426102110
Shibly <shibly2016@gmail.com>
2020-05-31 06:47:50 +0530

    Updates

---
+++ precache-manifest.046a45e4a7a6ee45d6bc3cffba0c1cb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d36b779073fa9fc858c2b099cc1025",
+    "url": "/calculater/index.html"
+  },
+  {
+    "revision": "ac0a7465337663d618e5",
+    "url": "/calculater/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "72d241342fb3fb35a37b",
+    "url": "/calculater/static/js/2.abb528ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculater/static/js/2.abb528ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac0a7465337663d618e5",
+    "url": "/calculater/static/js/main.d4e5ae2c.chunk.js"
+  },
+  {
+    "revision": "45c7fddd50a6921153f2",
+    "url": "/calculater/static/js/runtime-main.16628af3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculater/static/media/logo.5d5d9eef.svg"
+  }
+]);
